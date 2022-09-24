9d30098c562a0eb611e3b94be3907c6a5de6c256
Gabriel <pgabrieldeveloper@outlook.com>
2022-09-24 13:46:22 -0300

    initial commit

---
+++ src/main/java/io/github/pgabrieldev/adjafebd/AdjafebdApplication.java
@@ -0,0 +1,13 @@
+package io.github.pgabrieldev.adjafebd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdjafebdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdjafebdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/pgabrieldev/adjafebd/AdjafebdApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pgabrieldev.adjafebd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdjafebdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
