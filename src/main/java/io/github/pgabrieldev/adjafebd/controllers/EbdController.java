f671b1041c45e4fc8ac4d901ff261a01e9f7ab82
Gabriel <pgabrieldeveloper@outlook.com>
2022-09-25 17:26:30 -0300

    criando controller

---
+++ src/main/java/io/github/pgabrieldev/adjafebd/controllers/EbdController.java
@@ -0,0 +1,36 @@
+package io.github.pgabrieldev.adjafebd.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.github.pgabrieldev.adjafebd.dtos.AlunoRequest;
+import io.github.pgabrieldev.adjafebd.dtos.ClasseRequest;
+import io.github.pgabrieldev.adjafebd.dtos.utils.HelpersDTO;
+import io.github.pgabrieldev.adjafebd.entities.Aluno;
+import io.github.pgabrieldev.adjafebd.entities.Classe;
+import io.github.pgabrieldev.adjafebd.services.facade.EbdFacade;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RequestMapping("adjafebd/v1/ebd")
+@RequiredArgsConstructor
+public class EbdController {
+    
+
+    private final EbdFacade service;
+
+    @PostMapping("/classe")
+    public Mono<Classe> saveClasse(@RequestBody ClasseRequest request) {
+        return service.saveClasse(HelpersDTO.classeRequestToClasseEntity(request));
+    }
+    @PostMapping("/aluno")
+    public Mono<Aluno> saveAluno(@RequestBody AlunoRequest request) {
+        return service.saveAluno(HelpersDTO.alunoRequestToAlunoEntity(request));
+    }
+    
+
+}
